baee7f7d6477a88534e1bf91e1a5418a0b9d4ede
Perez Sen <103560740+PerezSen@users.noreply.github.com>
2026-01-01 01:13:27 -0600

    Update tweenShaderFloat callback with comment wd

--- source/psychlua/ShaderFunctions.hx
+++ source/psychlua/ShaderFunctions.hx
@@ -312,11 +312,11 @@ class ShaderFunctions
     	Lua_helper.add_callback(lua, "cancelShaderTween", function(tag:String, prop:String) {
 		    var tweenTag:String = tag + "_" + prop;
 		    var modchartTweens = PlayState.instance.modchartTweens;
-		    var twn:FlxTween = modchartTweens.get(tweenTag);
+		    var twn = modchartTweens.get(tweenTag);
 		        if(twn != null) {
 		            twn.cancel();
 		            twn.destroy();
-					modchartTweens.remove(tweenTag); 
+					modchartTweens.remove(tweenTag);  
 				}
 		});
 	}
